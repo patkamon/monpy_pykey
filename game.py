d30241d9d059033f465e8374b7e23b6ffc2523fc
patkamon <patkamon.a@ku.th>
2021-03-09 11:07:36 +0700

    S

--- game.py
+++ game.py
@@ -4,6 +4,9 @@ import tkinter.ttk as ttk
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 500
 
+UPDATE_DELAY =33
+GRAVITY = 1
+
 class Sprite():
 
     def __init__(self,canvas,image_filename,x=0,y=0):
@@ -13,10 +16,35 @@ class Sprite():
         self.canvas = canvas
 
         self.init_canvas_object()
+        self.init_sprite()
+
+    def init_sprite(self):
+        pass
 
     def init_canvas_object(self):
         self.photo_image = tk.PhotoImage(file=self.image_filename)
         self.canvas_object_id = self.canvas.create_image(self.x,self.y,image = self.photo_image)
+
+    def render(self):
+        self.canvas.coords(self.canvas_object_id,self.x,self.y)
+
+    def update(self):
+        pass
+
+class Banana(Sprite):
+
+    def init_sprite(self):
+        self.vx = 0
+        self.vy = 0
+
+    def set_speed(self,vx,vy):
+        self.vx = vx
+        self.vy = vy
+
+    def update(self):
+        self.x +=5
+        self.y -= self.vy
+        self.vy -= GRAVITY
 
 
 class MonkeyGame(ttk.Frame):
@@ -33,8 +61,17 @@ class MonkeyGame(ttk.Frame):
         self.canvas.grid(sticky="NEWS")
 
     def create_sprite(self):
-        self.banana = Sprite(self.canvas,'banana.png',100,100)
+        self.banana = Banana(self.canvas,'banana.png',100,400)
+        self.banana.set_speed(15,25)
 
+    def animate(self):
+        self.banana.update()
+        self.banana.render()
+
+        self.after(UPDATE_DELAY,self.animate)
+
+    def start(self):
+        self.after(0,self.animate)
 
 
 
@@ -44,4 +81,5 @@ if __name__ == '__main__':
 
     root.resizable(False,False)
     app = MonkeyGame(root)
+    app.start()
     root.mainloop()
