574f855ecb366604c5c858721002ed91b7ac7315
patkamon <patkamon.a@ku.th>
2021-03-09 10:09:11 +0700

    Extract sprite Class Resolve #1

--- game.py
+++ game.py
@@ -4,19 +4,36 @@ import tkinter.ttk as ttk
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 500
 
+class Sprite():
+
+    def __init__(self,canvas,image_filename,x=0,y=0):
+        self.x = x
+        self.y = y
+        self.image_filename = image_filename
+        self.canvas = canvas
+
+        self.init_canvas_object()
+
+    def init_canvas_object(self):
+        self.photo_image = tk.PhotoImage(file=self.image_filename)
+        self.canvas_object_id = self.canvas.create_image(self.x,self.y,image = self.photo_image)
+
 
 class MonkeyGame(ttk.Frame):
+
     def __init__(self,parent):
         super().__init__(parent)
         self.grid(sticky='news')
         self.create_widget()
 
+        self.create_sprite()
+
     def create_widget(self):
         self.canvas = tk.Canvas(self,borderwidth = 0,width = CANVAS_WIDTH,height  = CANVAS_HEIGHT,highlightthickness = 0)
         self.canvas.grid(sticky="NEWS")
 
-        self.banana_image = tk.PhotoImage(file='banana.png')
-        self.banana = self.canvas.create_image(100,100,image=self.banana_image)
+    def create_sprite(self):
+        self.banana = Sprite(self.canvas,'banana.png',100,100)
 
 
 
